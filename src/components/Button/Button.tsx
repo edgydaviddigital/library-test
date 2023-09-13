fdc98ed1f634e57d8918e6105a8a15040b21cd5c
edgydaviddigital <git-commit-address>
2023-09-13 14:20:02 +0200

    tailwind implementation

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,8 +3,17 @@ import "./Button.css";
 
 interface Props {
   label: string;
+  className?: string;
+  onClick?: () => void;
 }
 
-export default function Button({ label }: Props) {
-  return <button>{label}</button>;
+export default function Button({ label, className, onClick }: Props) {
+  return (
+    <button
+      onClick={onClick}
+      className={`bg-black text-white px-[18px] h-[54px] rounded-xl hover:scale-[1.1] transition-all duration-[300ms] ${className}`}
+    >
+      {label}
+    </button>
+  );
 }
